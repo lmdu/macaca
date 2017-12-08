2db1f7f04a071147fcffc4564ce389d5667571e1
Lianming Du <adullb@qq.com>
2017-12-08 15:59:49 +0800

    added semantic ui

--- snpdb/views.py
+++ snpdb/views.py
@@ -3,7 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Species
+from .models import Variant
+
 # Create your views here.
 def index(request):
-	return render(request, 'snpdb/index.html')
+	variants = Variant.objects.all()[:10]
+	return render(request, 'snpdb/index.html', {'snps': variants})
 	
